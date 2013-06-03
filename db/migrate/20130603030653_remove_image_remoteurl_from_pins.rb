be526e46f40d0fa87f53a1ce7479e9033871c91e
Joshua Kaplan <ylkaplan@gmail.com>
2013-06-02 23:26:13 -0400

    Add Image from URL upload2

--- db/migrate/20130603030653_remove_image_remoteurl_from_pins.rb
+++
@@ -1,9 +0,0 @@
-class RemoveImageRemoteurlFromPins < ActiveRecord::Migration
-  def up
-    remove_column :pins, :image_remoteurl
-  end
-
-  def down
-    add_column :pins, :image_remoteurl, :string
-  end
-end
